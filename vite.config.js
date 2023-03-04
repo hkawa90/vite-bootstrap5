058fb6c003e631c954483d8854e57e3db0bc2ac6
hkawa90 <hideo.90.kawamura@gmail.com>
2023-03-04 19:25:01 +0900

    add build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import path from 'path'
 export default {
   root: path.resolve(__dirname, 'src'),
+  build: {
+    rollupOptions: {
+      output:
+      {
+        format: 'es',
+        strict: false,
+        entryFileNames: "[name].js",
+        dir: 'dist/'
+      }
+    }
+  },
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
